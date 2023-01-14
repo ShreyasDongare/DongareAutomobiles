f74e1051a69712ec7fd36888baf5b913c9729913
Shreyas <dongareshreyas@gmail.com>
2023-01-14 20:13:21 +0530

    favicon and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,28 +18,32 @@ const About = () => {
           <div className="flex  ">
             <h2 className="text-5xl font-mono flex items-center">1965</h2>
             <div className="bg-rose-700 w-1  mx-4 rounded-xl h"></div>
-            <p className='flex items-center'>
+            <p className="flex items-center">
               {currrentYear - 1 - 1965}+ Years of experience in car repair and
               maintenance services.
             </p>
           </div>
           <p className="my-4">
-            Dongare Automobiles provides a wide range of Car services to
-            customers looking for reliable car maintenance services. We provide
-            exceptional Service in Sangli that focuses on your car needs. We
-            respect every vehicle as if it were our own.
+            Dongare Automobiles was established by Manohar Mahadeo Dongare in year 1965. We provides a wide range of Car
+            services to customers looking for reliable car maintenance services.
+            We provide exceptional Service in Sangli that focuses on your car
+            needs. We respect every vehicle as if it were our own.
           </p>
           <p className="my-4">
             Dongare Automobiles Workshop is your one-stop-shop for all car
             repair and maintenance needs, including car servicing, car repair,
             and custom car care. No task is too big or small for our Dongare
-            Automobiles team, whether you have a damaged fan belt, oil change or need to
-            replace your brake pads. For our customers, we provide excellent
-            service and value for money. We provide the same level of care for
-            your car, ensuring that your service warranty is not canceled. You
-            can trust our team to diagnose and repair your car.
+            Automobiles team, whether you have a damaged fan belt, oil change or
+            need to replace your brake pads. For our customers, we provide
+            excellent service and value for money. We provide the same level of
+            care for your car, ensuring that your service warranty is not
+            canceled. You can trust our team to diagnose and repair your car.
           </p>
-          <button  className='px-4 py-2 bg-cyan-600 hover:bg-cyan-800 duration-300 rounded'><a className='' href="tel:9637212336">Call Us</a></button>
+          <button className="px-4 py-2 bg-cyan-600 hover:bg-cyan-800 duration-300 rounded">
+            <a className="" href="tel:9637212336">
+              Call Us
+            </a>
+          </button>
         </div>
       </div>
     </section>
